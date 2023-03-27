56eaca71190130c57e42a995d25f603e456895c0
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-27 13:22:37 +0200

    Update page style.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,21 @@
 import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import {getAllPostIds, getPostData} from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css"
 
 export default ({postData}) => {
   return <Layout>
     <Head>
       <title>{postData.title}</title>
     </Head>
-    <h1>{postData.title}</h1>
-    <br/>
-    {postData.id}
-    <br/>
-    {postData.date}
-    <br />
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    <article>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date}/>
+      </div>
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+    </article>
   </Layout>
 }
 
