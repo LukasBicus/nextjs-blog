2b795e3f0f29e140534ab4d1874fd5d3cfcaa9e8
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-27 09:41:55 +0200

    Create new layout.

--- pages/page-with-event.js
+++ pages/page-with-event.js
@@ -1,19 +1,11 @@
-import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 export default () => {
-  return <div className={styles.container}>
-    <Head>
-      <title>Page with iframe</title>
-      <link rel="icon" href="/favicon.ico"/>
-    </Head>
-
+  return <Layout>
     <h1>Page with event detail</h1>
-    <h2>
-      <Link href="/">Back to home</Link>
-    </h2>
-    <iframe src={'http://localhost:3001/event/1'} width="90%" height="600px">
-    </iframe>
-  </div>
+    <section>
+      <iframe src={'http://localhost:3001/event/1'} width="90%" height="600px">
+      </iframe>
+    </section>
+  </Layout>
 }
--- pages/page-with-list.js
+++ pages/page-with-list.js
@@ -1,19 +1,11 @@
-import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 export default () => {
-  return <div className={styles.container}>
-    <Head>
-      <title>Page with iframe</title>
-      <link rel="icon" href="/favicon.ico"/>
-    </Head>
-
+  return <Layout>
     <h1>Page with event list</h1>
-    <h2>
-      <Link href="/">Back to home</Link>
-    </h2>
-    <iframe src={'http://localhost:3001/2/events?limit=10'} width="90%" height="600px">
-    </iframe>
-  </div>
+    <section>
+      <iframe src={'http://localhost:3001/2/events?limit=10'} width="90%" height="600px">
+      </iframe>
+    </section>
+  </Layout>
 }
