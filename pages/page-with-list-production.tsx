255d9a4d1f4ae98fc3fe06d04b2a64283dbaafd7
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-29 14:21:04 +0200

    Update production 2

--- pages/page-with-list-production.tsx
+++ pages/page-with-list-production.tsx
@@ -6,8 +6,8 @@ export default () => {
     <section>
       <iframe src={'https://shop.entradio.sk/2/events?limit=10'} width="90%" height="600px">
       </iframe>
-      <iframe src={'https://shop.entradio.sk/event/11'} width="90%" height="600px">
-      </iframe>
+      {/*<iframe src={'https://shop.entradio.sk/event/11'} width="90%" height="600px">*/}
+      {/*</iframe>*/}
     </section>
   </Layout>
 }
