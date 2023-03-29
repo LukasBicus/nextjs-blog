5bfa5eeb23955239dce03c06a1dad0a8a9a8cfe8
lukasbicus <ing.lukas.bicus@gmail.com>
2023-03-29 14:13:42 +0200

    Update production

--- pages/page-with-list-production.tsx
+++ pages/page-with-list-production.tsx
@@ -4,7 +4,9 @@ export default () => {
   return <Layout>
     <h1>Page with event list on production</h1>
     <section>
-      <iframe src={'https://shop.entradio.sk/11/events?limit=10'} width="90%" height="600px">
+      <iframe src={'https://shop.entradio.sk/2/events?limit=10'} width="90%" height="600px">
+      </iframe>
+      <iframe src={'https://shop.entradio.sk/event/11'} width="90%" height="600px">
       </iframe>
     </section>
   </Layout>
