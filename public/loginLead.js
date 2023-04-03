dd49a227681d357c7ab1459fc320b921d323432a
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-03 14:12:00 +0200

    Update loginLead.js 7

--- public/loginLead.js
+++ public/loginLead.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   console.log('running the request')
   // fetch('https://shop.entradio.sk/api/graphql', {
-  fetch('http://localhost:4000/graphql', {
+  fetch('https://localhost:4000/graphql', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
