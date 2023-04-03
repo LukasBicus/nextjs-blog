cac7d07bc63cc94b619daf63dcddeeb7fee28c07
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-03 13:21:52 +0200

    Add page with script

--- public/loginLead.js
+++ public/loginLead.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   console.log('running the request')
-  fetch('https://shop.entradio.sk/api/graphql', {
+  // fetch('https://shop.entradio.sk/api/graphql', {
+  fetch('http://localhost:3001/2/events', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -25,13 +26,15 @@ function handleLoginSuccess(result) {
   const parentCartDiv = document.getElementById('iframe-shop-entradio-cart')
 
   const iframeEventElement = document.createElement('iframe')
-  iframeEventElement.src = 'https://shop.entradio.sk/event/9'
+  // iframeEventElement.src = 'https://shop.entradio.sk/event/9'
+  iframeEventElement.src = 'http://localhost:3001/event/1'
   iframeEventElement.width = '100%'
   iframeEventElement.height = '600px'
   parentEventDiv.appendChild(iframeEventElement)
 
   const iframeCartElement = document.createElement('iframe')
-  iframeCartElement.src = 'https://shop.entradio.sk/2/cart'
+  // iframeCartElement.src = 'https://shop.entradio.sk/2/cart'
+  iframeCartElement.src = 'http://localhost:3001/2/events'
   iframeCartElement.width = '100%'
   iframeCartElement.height = '600px'
   parentCartDiv.appendChild(iframeCartElement)
