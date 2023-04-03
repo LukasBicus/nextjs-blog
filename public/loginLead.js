44689caf1502de9987beccb00ec7e2cfccfa102f
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-03 14:02:19 +0200

    Update loginLead.js 6

--- public/loginLead.js
+++ public/loginLead.js
@@ -15,8 +15,9 @@ window.onload = function () {
       variables: {}
     })
   }).then((res) => {
-
-    return res.json()})
+    console.log('res', res)
+    return res.json()
+  })
     .then((result) => handleLoginSuccess(result))
     .catch((error) => console.log(error))
 }
@@ -24,20 +25,20 @@ window.onload = function () {
 function handleLoginSuccess(result) {
   console.log(result)
 
-  const parentEventDiv = document.getElementById('iframe-shop-entradio-event')
-  const parentCartDiv = document.getElementById('iframe-shop-entradio-cart')
+  // const parentEventDiv = document.getElementById('iframe-shop-entradio-event')
+  // const parentCartDiv = document.getElementById('iframe-shop-entradio-cart')
 
-  const iframeEventElement = document.createElement('iframe')
-  // iframeEventElement.src = 'https://shop.entradio.sk/event/9'
-  iframeEventElement.src = 'http://localhost:3001/event/1'
-  iframeEventElement.width = '100%'
-  iframeEventElement.height = '600px'
-  parentEventDiv.appendChild(iframeEventElement)
-
-  const iframeCartElement = document.createElement('iframe')
-  // iframeCartElement.src = 'https://shop.entradio.sk/2/cart'
-  iframeCartElement.src = 'http://localhost:3001/2/events'
-  iframeCartElement.width = '100%'
-  iframeCartElement.height = '600px'
-  parentCartDiv.appendChild(iframeCartElement)
+  // const iframeEventElement = document.createElement('iframe')
+  // // iframeEventElement.src = 'https://shop.entradio.sk/event/9'
+  // iframeEventElement.src = 'http://localhost:3001/event/1'
+  // iframeEventElement.width = '100%'
+  // iframeEventElement.height = '600px'
+  // parentEventDiv.appendChild(iframeEventElement)
+  //
+  // const iframeCartElement = document.createElement('iframe')
+  // // iframeCartElement.src = 'https://shop.entradio.sk/2/cart'
+  // iframeCartElement.src = 'http://localhost:3001/2/events'
+  // iframeCartElement.width = '100%'
+  // iframeCartElement.height = '600px'
+  // parentCartDiv.appendChild(iframeCartElement)
 }
