77ce31315e4c6d47f37bbfd868ff0cd964a81002
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-21 15:53:14 +0200

    Update to https://...

--- pages/page-with-event.tsx
+++ pages/page-with-event.tsx
@@ -4,7 +4,7 @@ export default () => {
   return <Layout>
     <h1>Page with event detail</h1>
     <section>
-      <iframe src={'http://localhost:3001/event/1'} width="90%" height="600px">
+      <iframe src={'https://localhost:3001/event/1'} width="90%" height="600px">
       </iframe>
     </section>
   </Layout>
