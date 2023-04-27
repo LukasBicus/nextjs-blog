6635c5dcadbf43a4cb2170d54616d3702242bce9
lukasbicus <ing.lukas.bicus@gmail.com>
2023-04-27 09:26:18 +0200

    Update page-with-list.tsx

--- pages/page-with-list.tsx
+++ pages/page-with-list.tsx
@@ -4,7 +4,7 @@ export default () => {
   return <Layout>
     <h1>Page with event list</h1>
     <section>
-      <iframe src={'https://localhost:3001/1/events?eventButtonTarget=_self&limit=10'} width="90%" height="600px">
+      <iframe src={'https://localhost:3001/2/events?eventButtonTarget=_self&limit=10'} width="90%" height="600px">
       </iframe>
     </section>
   </Layout>
